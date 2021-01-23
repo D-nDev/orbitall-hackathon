29be965348a7ffe3c4ffd1869d9a7c049be9407a
D-nDev <diego-s.novaes@hotmail.com>
2021-01-23 15:09:44 -0300

    Node adicionado

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -1,7 +1,88 @@
 const neDB = require('../configurations/database')
 const api = {}
 
+api.findOne = (request, response) => {
+
+    let x = request.params._id
+    neDB.findOne({ _id: x }, (exception, customer ) => {
+
+        if(exception) {
+            const sentence = "Error";
+            console.log(sentence, exception)
+            response.status(exception.status | 400)
+            response.json({'mensagem': sentence})
+        }
+        response.json(customer)
+    } )
+}
+
+api.update = (request, response) => {
+
+    let x = request.params._id
+    let card2 = request.body.cardNumber
+    let customer2 = request.body.customerName
+    let document2 = request.body.documentNumber
+    let mother2 = request.body.motherName
+    let city2 = request.body.city
+    let address2 = request.body.address
+    neDB.update({ _id: x  }, { $set: { cardNumber: card2, customerName: customer2, documentNumber: document2, motherName: mother2, city: city2, address: address2 } }, { multi: true }, (exception, customer ) => {
+
+        if(exception) {
+            const sentence = "Error";
+            console.log(sentence, exception)
+            response.status(exception.status | 400)
+            response.json({'mensagem': sentence})
+        }
+        response.json("Client updated")
+    } )
+}
+
+api.remove = (request, response) => {
+
+    let x = request.params._id
+    neDB.remove({ _id: x }, (exception, customer ) => {
+
+        if(exception) {
+            const sentence = "Error";
+            console.log(sentence, exception)
+            response.status(exception.status | 400)
+            response.json({'mensagem': sentence})
+        }
+        response.json("User has been deleted")
+    } )
+}
+
 api.findAll = (request, response) => {
+    neDB.find({}).sort({name: 1}).exec((exception, cards) => {
+        if(exception) {
+            const sentence = 'Error on show all clients!'
+            console.log(sentence, exception)
+
+            response.status(exception.status | 400)
+            response.json({'mensagem': sentence})
+        }
+        response.json(cards)
+    })
+
+}
+
+api.save = (request, response) => {
+    const canonical = request.body
+
+    neDB.insert(canonical, (exception, card) => {
+        if(exception) {
+            const sentence2 = 'Error on show a card!'
+            console.log(sentence2, exception)
+
+            response.status(exception.status | 400)
+            response.json({'mensagem': sentence2})
+        }
+
+        response.json(card)
+        response.status(201)
+    })
+
+
 }
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,16 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
+        .post(api.save)
 
+    app.route('/cards/:_id')
+        .get(api.findOne)
+
+    app.route('/cards/:_id')
+        .delete(api.remove)
+
+    app.route('/cards/:_id')
+        .put(api.update)
 }
